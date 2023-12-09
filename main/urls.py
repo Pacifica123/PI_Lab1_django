30800bcf391732c61ced4e976c707a4a0998fc77
noir <rion123tea@gmail.com>
2023-12-09 21:16:08 +0700

    решил проблему с фотками

--- main/urls.py
+++ main/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import index
+from .views import index, serve_image 
 
 urlpatterns = [
-    path('index/', index, name='index')
+    path('index/', index, name='index'),
+    path('media/photos/<str:image_path>/', serve_image, name='serve_image')
 ]
--- main/views.py
+++ main/views.py
@@ -1,7 +1,18 @@
 from django.shortcuts import render, redirect
 from .models import Product
+from django.http import HttpResponse
+from django.conf import settings
+import os
 
 # Create your views here.
 def index(request):
     products = Product.objects.all()
-    return render(request, 'main/index.html', {'products': products})
+    context = {'products': products, 'media_root': settings.MEDIA_ROOT, 'MEDIA_URL': settings.MEDIA_URL}
+    return render(request, 'main/index.html', context=context)
+
+def serve_image(request, image_path):
+    image_path = os.path.join(settings.MEDIA_ROOT, image_path)
+    if os.path.exists(image_path):
+        with open(image_path, 'rb') as img:
+            return HttpResponse(img.read(), content_type="image/jpeg")  # Уточни тип содержимого в соответствии с расширением файла
+    return HttpResponse(status=404)
